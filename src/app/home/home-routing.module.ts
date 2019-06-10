26b5e095db9153ef8e6f1484e3b546f9056e7c17
FreedomKhanyile <freedom.khanyile1@gmail.com>
2019-06-10 19:58:30 +0200

    Home works
    Basic routing

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent,
+    children: [
+      {
+        path: "",
+        component: HomeComponent
+      }
+    ]
+  }
+];
 
+export const declarations: Array<any> = [
+  HomeComponent
+]
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 export class HomeRoutingModule { }
