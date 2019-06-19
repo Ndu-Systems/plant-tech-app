7922503ce871679711e9ebbb4fabd34c37c41730
FreedomKhanyile <freedom.khanyile1@gmail.com>
2019-06-20 00:01:52 +0200

    WIP
    New  list of plants on front page

--- src/app/home/Index/top-featured/top-featured.component.ts
+++ src/app/home/Index/top-featured/top-featured.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Plant } from 'src/app/models/plant';
 
 @Component({
   selector: 'app-top-featured',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./top-featured.component.scss']
 })
 export class TopFeaturedComponent implements OnInit {
+  plants: Plant[] = [
+    {
+      PlantId: '33c5f566-8d59-11e9-bd99-80fa5b45280e',
+      Name: 'Plant 1',
+      Description: 'Test Dummy 1',
+      Views: 1,
+      MedicineId: '1',
+      CreateUserId: 'sys',
+      CreateDate: new Date('2019-06-12 23:29:47'),
+      ModifyUserId: 'sys_update',
+      ModifyDate: new Date('2019-06-13 06:48:28'),
+      StatusId: 1
+    },
+    {
+      PlantId: '4fef47c2-8d59-11e9-bd99-80fa5b45280e',
+      Name: 'Plant 2',
+      Description: 'Test Dummy 2',
+      Views: 1,
+      MedicineId: '1',
+      CreateUserId: 'sys',
+      CreateDate: new Date('2019-06-12 23:29:47'),
+      ModifyUserId: 'sys_update',
+      ModifyDate: new Date('2019-06-13 06:48:28'),
+      StatusId: 1
+    }
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
---
+++ src/app/models/plant.ts
@@ -0,0 +1,12 @@
+export class Plant {
+  PlantId: string;
+  Name: string;
+  Description: string;
+  Views: number;
+  MedicineId: string;
+  CreateUserId: string;
+  CreateDate: Date;
+  ModifyUserId: string;
+  ModifyDate: Date;
+  StatusId: number;
+}
