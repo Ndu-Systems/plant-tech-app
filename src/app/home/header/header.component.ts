6f9d08746036c95f72b98324030a3cbe38df9b76
FreedomKhanyile <freedom.khanyile1@gmail.com>
2019-07-05 19:40:22 +0200

    home page styling navigation bar and background

--- src/app/home/header/header.component.ts
+++ src/app/home/header/header.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Inject, HostListener } from "@angular/core";
 import { NavigationProcessService } from "src/app/services/app-state";
-
+import { DOCUMENT } from "@angular/platform-browser";
 
 @Component({
   selector: "app-header",
@@ -8,23 +8,36 @@ import { NavigationProcessService } from "src/app/services/app-state";
   styleUrls: ["./header.component.scss"]
 })
 export class HeaderComponent implements OnInit {
-
   showNav: boolean;
-  constructor(private navigationProcessService: NavigationProcessService) {}
+  constructor(
+    private navigationProcessService: NavigationProcessService,
+    @Inject(DOCUMENT) document
+  ) {}
 
   ngOnInit() {
     this.navigationProcessService.castNavigationProcess.subscribe(process => {
       this.showNav = process.showNav;
-
     });
   }
 
   // open navigation.
   openNav() {
     this.navigationProcessService.showNav();
+    let element = document.getElementById("header-home");
+    element.classList.remove("sticky");
   }
 
-  closeNav(){
+  closeNav() {
     this.navigationProcessService.closeNav();
+  }
+  @HostListener("window:scroll", ["$event"])
+  onWindowScroll(e) {
+    if (window.pageYOffset > 550) {
+      let element = document.getElementById("header-home");
+      element.classList.add("sticky");
+    } else {
+      let element = document.getElementById("header-home");
+      element.classList.remove("sticky");
+    }
   }
 }
